67ce1409ffccb4ab4cc20fa3454185c979d7b774
IrynaKr1 <krutsiryna@gmail.com>
2026-05-02 14:34:14 +0200

    Add user-game seed

---
+++ server/db/seeders/20260502122444-seed-user-games.js
@@ -0,0 +1,55 @@
+'use strict';
+const { GAME_STATUS } = require('../../constants');
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    const [users] = await queryInterface.sequelize.query(
+      `SELECT id FROM users WHERE email = 'ira@example.com';`
+    );
+    const userId = users[0].id;
+
+    await queryInterface.bulkInsert('user_games', [
+      {
+        user_id: userId,
+        rawg_id: 326243,
+        status: GAME_STATUS.IN_PROGRESS,
+        rating: null,
+        playtime: 80,
+        created_at: new Date(),
+        updated_at: new Date(),
+      },
+      {
+        user_id: userId,
+        rawg_id: 3328,
+        status: GAME_STATUS.COMPLETED,
+        rating: 4.8,
+        playtime: 50,
+        created_at: new Date(),
+        updated_at: new Date(),
+      },
+      {
+        user_id: userId,
+        rawg_id: 58175,
+        status: GAME_STATUS.IN_PROGRESS,
+        rating: null,
+        playtime: 30,
+        created_at: new Date(),
+        updated_at: new Date(),
+      },
+      {
+        user_id: userId,
+        rawg_id: 41494,
+        status: GAME_STATUS.NOT_STARTED,
+        rating: null,
+        playtime: 60,
+        created_at: new Date(),
+        updated_at: new Date(),
+      },
+    ]);
+  },
+
+  async down (queryInterface, Sequelize) {
+    await queryInterface.bulkDelete('user_games', null, {});
+  },
+};
