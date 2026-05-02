ee3ed9ba01d80eb67ed978d4bde243adf11624f6
IrynaKr1 <krutsiryna@gmail.com>
2026-05-02 16:19:12 +0200

    Add getAllGames to controller

--- server/controllers/gamesController.js
+++ server/controllers/gamesController.js
@@ -1,4 +1,27 @@
-module.exports.getAllGames = async (req, res, next) => {};
+const { UserGame, GameNote } = require('../db/models');
+
+const USER_ID = 1;
+
+module.exports.getAllGames = async (req, res, next) => {
+  try {
+    const games = await UserGame.findAll({
+      raw: true,
+      where: { user_id: USER_ID },
+      include: {
+        model: GameNote,
+        as: 'notes',
+      },
+      attributes: {
+        exclude: ['createdAt', 'updatedAt'],
+      },
+    });
+
+    res.status(200).send({ data: games });
+  } catch (error) {
+    next(error);
+  }
+};
+
 module.exports.getGameById = async (req, res, next) => {};
 module.exports.addGame = async (req, res, next) => {};
 module.exports.updateGameById = async (req, res, next) => {};
