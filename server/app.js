5f2ef546d496e8b41edcf5964009754bb76edb25
IrynaKr1 <krutsiryna@gmail.com>
2026-05-02 10:51:48 +0200

    Add sequelize configs

--- server/app.js
+++ server/app.js
@@ -1,7 +1,15 @@
 const express = require('express');
+const cors = require('cors');
 const router = require('./routes');
+const { errorHandler } = require('./middleware');
 
 const app = express();
+
+const corsOptions = {
+  origin: '*',
+};
+
+app.use(cors(corsOptions));
 
 app.use(express.json());
 
