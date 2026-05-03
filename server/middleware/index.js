37f8693876aead5a5b03928d9876e29444647469
IrynaKr1 <krutsiryna@gmail.com>
2026-05-03 16:53:09 +0200

    Add upload img

--- server/middleware/index.js
+++ server/middleware/index.js
@@ -1,1 +1,2 @@
 module.exports.errorHandler = require('./errorHandlers');
+module.exports.upload = require('./upload');
--- server/routes/gamesRouter.js
+++ server/routes/gamesRouter.js
@@ -1,17 +1,18 @@
 const { Router } = require('express');
 const { gamesController } = require('../controllers');
+const { upload } = require('../middleware');
 
 const gamesRouter = Router();
 
 gamesRouter
   .route('/')
   .get(gamesController.getAllGames)
-  .post(gamesController.addGame);
+  .post(upload.uploadGameImage, gamesController.addGame);
 
 gamesRouter
   .route('/:id')
   .get(gamesController.getGameById)
-  .patch(gamesController.updateGameById)
+  .patch(upload.uploadGameImage, gamesController.updateGameById)
   .delete(gamesController.deleteGameById);
 
 module.exports = gamesRouter;
