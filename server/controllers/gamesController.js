8ba0c99a5480b5816162fc51b13a16431fd8f15f
IrynaKr1 <krutsiryna@gmail.com>
2026-05-04 19:31:04 +0200

    Add deleteGameById to controller

--- server/controllers/gamesController.js
+++ server/controllers/gamesController.js
@@ -1,6 +1,39 @@
+const createHttpError = require('http-errors');
+const _ = require('lodash');
+const fs = require('fs');
 const { UserGame, GameNote } = require('../db/models');
 
 const USER_ID = 1;
+
+module.exports.addGame = async (req, res, next) => {
+  const { body, file } = req;
+  try {
+    if (file) {
+      body.image = file.filename;
+    }
+
+    body.userId = USER_ID;
+
+    const createdGame = await UserGame.create({
+      userId: USER_ID,
+      title: body.title,
+      genre: body.genre,
+      status: body.status,
+      playtime: body.playtime,
+      image: file ? file.filename : null,
+    });
+
+    if (!createdGame) {
+      return next(createHttpError(400, 'Something went wrong'));
+    }
+
+    const preparedGame = _.omit(createdGame.get(), ['createdAt', 'updatedAt']);
+
+    res.status(201).send({ data: preparedGame });
+  } catch (error) {
+    next(error);
+  }
+};
 
 module.exports.getAllGames = async (req, res, next) => {
   try {
@@ -21,8 +54,24 @@ module.exports.getAllGames = async (req, res, next) => {
     next(error);
   }
 };
-
 module.exports.getGameById = async (req, res, next) => {};
-module.exports.addGame = async (req, res, next) => {};
 module.exports.updateGameById = async (req, res, next) => {};
-module.exports.deleteGameById = async (req, res, next) => {};
+module.exports.deleteGameById = async (req, res, next) => {
+  try {
+    const game = await UserGame.findByPk(req.params.id);
+
+    if (!game || game.userId !== USER_ID) {
+      return next(createHttpError(404, 'Game not found'));
+    }
+
+    if (game.image) {
+      const imagePath = path.join(STATIC_PATH, 'images', game.image);
+      fs.unlink(imagePath, () => {});
+    }
+    await game.destroy();
+
+    res.json({ message: 'A game was deleted' });
+  } catch (error) {
+    next(error);
+  }
+};
