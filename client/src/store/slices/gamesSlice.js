153d5c6b604b349798235b505a1782383ba49f8d
IrynaKr1 <krutsiryna@gmail.com>
2026-05-15 23:02:36 +0200

    Add createGameThunk

--- client/src/store/slices/gamesSlice.js
+++ client/src/store/slices/gamesSlice.js
@@ -9,6 +9,20 @@ const initialState = {
   error: null,
 };
 
+export const createGameThunk = createAsyncThunk(
+  `${GAMES_SLICE_NAME}/addGame`,
+  async (payload, { rejectWithValue }) => {
+    try {
+      const {
+        data: { data },
+      } = await API.createGame(payload);
+      return data;
+    } catch (error) {
+      return rejectWithValue({ errors: error.response.data });
+    }
+  }
+);
+
 export const getGameThunk = createAsyncThunk(
   `${GAMES_SLICE_NAME}/getAll`,
   async (payload, { rejectWithValue }) => {
@@ -28,6 +42,23 @@ const gameSlice = createSlice({
   initialState,
   reducers: {},
   extraReducers: builder => {
+    //add game
+
+    builder.addCase(createGameThunk.pending, state => {
+      state.isFetching = true;
+      state.error = null;
+    });
+
+    builder.addCase(createGameThunk.fulfilled, (state, { payload }) => {
+      state.isFetching = false;
+      state.games.push(payload);
+    });
+
+    builder.addCase(createGameThunk.rejected, (state, { payload }) => {
+      state.error = payload;
+      state.isFetching = false;
+    });
+
     // getAll
     builder.addCase(getGameThunk.pending, state => {
       state.isFetching = true;
@@ -40,8 +71,8 @@ const gameSlice = createSlice({
     });
 
     builder.addCase(getGameThunk.rejected, (state, { payload }) => {
+       state.error = payload;
       state.isFetching = false;
-      state.error = payload;
     });
   },
 });
